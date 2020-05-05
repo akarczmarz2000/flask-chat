b96d3d4fe2c88e4d11083509256485be57e00f26
Armand Józef Oliver Karczmarz <akarczmarz2000@gmail.com>
2020-05-05 02:16:50 +0000

    Preparing for Heroku deployment

--- run.py
+++ run.py
@@ -3,7 +3,7 @@ from datetime import datetime
 from flask import Flask, redirect, render_template, request, session, url_for
 
 app = Flask(__name__)
-app.secret_key = "randomstring123"
+app.secret_key = os.getenv("SECRET", "randomstring123")
 messages = []
 
 def add_message(username, message):
@@ -30,4 +30,4 @@ def user(username):
         return redirect(url_for("user", username=session["username"]))
     return render_template("chat.html", username = username, chat_messages = messages)
 
-app.run(host=os.getenv("IP"), port=int(os.getenv("PORT")), debug=True)
+app.run(host=os.getenv("IP", "0.0.0.0"), port=int(os.getenv("PORT", "5000")), debug=False)
